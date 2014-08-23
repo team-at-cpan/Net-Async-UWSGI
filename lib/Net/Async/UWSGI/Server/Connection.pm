af2b20dda06fddfc5fbf8ec332edfe97ceb698f7
Tom Molesworth <tom@perlsite.co.uk>
2014-08-23 15:21:58 +0000

    Cleanup
    git-svn-id: http://entitymodel.com/svn/trunk/Net-Async-UWSGI@2497 c5bf8c3b-bf20-4ed3-b413-d5547a49f493

--- lib/Net/Async/UWSGI/Server/Connection.pm
+++ lib/Net/Async/UWSGI/Server/Connection.pm
@@ -342,86 +342,25 @@ Have T-E: Chunked:
 'HTTP_ACCEPT_LANGUAGE' => 'en-GB,en-US;q=0.8,en;q=0.6',
 'REMOTE_PORT' => '57574',
 'PATH_INFO' => '/1.6.0/test',
-'HTTP_HOST' => 'midvale.lewisham.rumah',
+'HTTP_HOST' => 'uwsgi.lewisham.rumah',
 'HTTP_CONNECTION' => 'keep-alive',
 'QUERY_STRING' => '',
 'HTTP_ACCEPT' => 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
 'CONTENT_TYPE' => '',
 'REQUEST_METHOD' => 'GET',
-'SERVER_NAME' => 'midvale.lewisham.rumah',
+'SERVER_NAME' => 'uwsgi.lewisham.rumah',
 'SERVER_PROTOCOL' => 'HTTP/1.1',
 'HTTP_ACCEPT_ENCODING' => 'gzip,deflate,sdch',
-'REQUEST_URI' => '/1.6.0/test',
+'REQUEST_URI' => '/test',
 'REMOTE_ADDR' => '192.168.1.1',
 'HTTP_CACHE_CONTROL' => 'max-age=0',
 'HTTP_USER_AGENT' => 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.1916.153 Safari/537.36',
 'SERVER_PORT' => '80',
 'CONTENT_LENGTH' => '',
 'UWSGI_SCHEME' => 'http',
-'DOCUMENT_ROOT' => '/home/tom/dev/TrendMicro/midvale1.6'
+'DOCUMENT_ROOT' => '/perl/somewhere'
 },
 =cut
-
-sub handle_packet {
-	my ($self, $env) = @_;
-	# warn "Have packet - " . Dumper($pkt);
-	my $uri = uri_from_env($env);
-	my $q = $uri->query_form_hash;
-	my %param;
-	for my $k (keys %$q) {
-		$param{$k} = [
-			map split(/[, ]/, $_),	# list separation characters
-				ref($q->{$k})		# expand arrayrefs
-				? @{$q->{$k}}
-				: ($q->{$k})
-		];
-	}
-	# IO::Async::Timer::Countdown
-	my $write = $self->curry::weak::write;
-	my $timer = IO::Async::Timer::Countdown->new(
-		delay => 0.3,
-		remove_on_expire => 1,
-        on_expire => sub {
-			my $raw = Encode::encode('UTF-8' => $self->json->encode({ some_data => 123 }));
-			my @http = (
-				'HTTP/1.1 200 OK',
-				'Host: localhost',
-				'Content-Type: application/javascript',
-				'Content-Length: ' . length($raw),
-				'',
-				$raw
-			);
-		#	say for @http;
-			$write->(
-				join "\015\012", @http
-			)->on_done(
-				$self->curry::weak::close
-			);
-        },
-	);
-
-	$timer->start;
-
-	$self->add_child( $timer );
-	# immediately starts the response and stream the content
-	return sub {
-		my $responder = shift;
-		my $writer = $responder->([
-			200, [
-				'Content-Type', 'application/json'
-			]
-		]);
-
-		wait_for_events(sub {
-			my $new_event = shift;
-			if ($new_event) {
-				$writer->write($new_event->as_json . "\n");
-			} else {
-				$writer->close;
-			}
-		});
-	};
-}
 
 1;
 
